67a0a72edb5df994842b5329309d97a34c3c4f8a
lockatmee <diahkhairunisa2005@gmail.com>
2026-06-16 22:36:46 +0700

    Menambahkan model Document

---
+++ src/main/java/id/adrianz/ruangkelas/model/Document.java
@@ -0,0 +1,53 @@
+
+package id.adrianz.ruangkelas.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "documents")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "class_id", nullable = false)
+    private Class clazz;
+
+    private String title;
+
+    private String fileName;
+
+    private String filePath;
+
+    private Long fileSize;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
