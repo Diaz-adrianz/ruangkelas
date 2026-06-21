b1c938d3ea3fb1fbf88e40c321b2390ac3db85f1
Dian <eka241502@gmail.com>
2026-06-21 22:53:29 +0700

    fitur tasksubmission dan controller sukses sinkron

--- src/main/java/id/adrianz/ruangkelas/controller/TaskSubmissionController.java
+++ src/main/java/id/adrianz/ruangkelas/controller/TaskSubmissionController.java
@@ -4,6 +4,7 @@ import id.adrianz.ruangkelas.model.TaskSubmission;
 import id.adrianz.ruangkelas.service.TaskSubmissionService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 @RestController
@@ -13,13 +14,18 @@ public class TaskSubmissionController {
     @Autowired
     private TaskSubmissionService submissionService;
 
-    @PostMapping
-    public TaskSubmission submitTask(@RequestBody TaskSubmission submission) {
-        return submissionService.submitTask(submission);
+    @PostMapping("/submit/{taskId}")
+    public ResponseEntity<TaskSubmission> submitTask (
+        @RequestBody TaskSubmission submission,
+        @PathVariable Long taskId
+    ){
+        TaskSubmission result = submissionService.submitTask(submission,taskId);
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping 
-    public List<TaskSubmission> getAllSubmissions(){
-        return submissionService.getAllSubmissions();
+    public ResponseEntity<List<TaskSubmission>> getAllSubmissions(){
+        List<TaskSubmission> submissions = submissionService.getAllSubmissions();
+        return ResponseEntity.ok(submissions);
     }
 }
