b46c4f74d11b4cd019a588ca484163b3ea2f88c7
Diaz-Adrianz <diazz.developer@gmail.com>
2026-06-21 19:47:39 +0700

    feat(email): sendTemplateMessage

--- src/main/java/id/adrianz/ruangkelas/service/EmailService.java
+++ src/main/java/id/adrianz/ruangkelas/service/EmailService.java
@@ -2,16 +2,21 @@ package id.adrianz.ruangkelas.service;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
-
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+    private final TemplateEngine templateEngine;
 
     public void sendSimpleMessage(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -19,5 +24,21 @@ public class EmailService {
         message.setSubject(subject);
         message.setText(text);
         mailSender.send(message);
+    }
+
+    // templatePath should located under resources/templates/email/
+    public void sendTemplateMessage(String to, String subject, String templatePath, Context context) {
+        String html = templateEngine.process(templatePath, context);
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(html, true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Gagal mengirim email: " + subject, e);
+        }
     }
 }
