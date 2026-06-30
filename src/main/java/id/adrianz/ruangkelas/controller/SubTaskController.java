3965fc9f2e9f397d60e96491c1f7cd5c9f131b5d
Diaz-Adrianz <diazz.developer@gmail.com>
2026-06-30 15:32:14 +0700

    fix(subtask): adjust edit page

--- src/main/java/id/adrianz/ruangkelas/controller/SubTaskController.java
+++ src/main/java/id/adrianz/ruangkelas/controller/SubTaskController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import id.adrianz.ruangkelas.dto.CreateSubTaskDto;
+import id.adrianz.ruangkelas.dto.UpdateSubTaskDto;
 import id.adrianz.ruangkelas.model.Class;
 import id.adrianz.ruangkelas.model.SubTask;
 import id.adrianz.ruangkelas.model.Task;
@@ -101,4 +102,67 @@ public class SubTaskController {
 
                 return "redirect:/class/" + classCode + "/tasks/" + taskId + "#subtasks";
         }
+
+        @GetMapping("/{subTaskId}/edit")
+        public String updateForm(@PathVariable String classCode,
+                        @PathVariable Long taskId,
+                        @PathVariable Long subTaskId,
+                        Model model) {
+
+                Class classs = classService.getByCode(classCode);
+                Task task = taskService.getTaskById(taskId);
+                SubTask subTask = subTaskService.getSubtaskById(subTaskId);
+
+                UpdateSubTaskDto dto = new UpdateSubTaskDto();
+                dto.setTitle(subTask.getTitle());
+                dto.setDescription(subTask.getDescription());
+                dto.setDeadline(subTask.getDeadline());
+
+                model.addAttribute("classs", classs);
+                model.addAttribute("task", task);
+                model.addAttribute("subtask", subTask);
+                model.addAttribute("updateSubTaskDto", dto);
+
+                return "pages/Subtask/Edit";
+        }
+
+        @PostMapping("/{subTaskId}/update")
+        public String edit(@PathVariable String classCode,
+                        @PathVariable Long taskId,
+                        @PathVariable Long subTaskId,
+                        @Valid @ModelAttribute("updateSubTaskDto") UpdateSubTaskDto request,
+                        BindingResult result,
+                        Model model,
+                        @AuthenticationPrincipal UserPrincipal principal,
+                        RedirectAttributes redirectAttributes) {
+
+                Class kelas = classService.getByCode(classCode);
+                Task task = taskService.getTaskById(taskId);
+                SubTask subTask = subTaskService.getSubtaskById(subTaskId);
+
+                if (result.hasErrors()) {
+                        model.addAttribute("classs", kelas);
+                        model.addAttribute("task", task);
+                        model.addAttribute("subtask", subTask);
+                        model.addAttribute("errors", result.getFieldErrors());
+                        return "pages/Subtask/Edit";
+                }
+
+                try {
+                        subTask.setTitle(request.getTitle());
+                        subTask.setDescription(request.getDescription());
+                        subTask.setDeadline(request.getDeadline());
+                        subTaskService.updateSubtask(subTask);
+                } catch (RuntimeException e) {
+                        model.addAttribute("classs", kelas);
+                        model.addAttribute("task", task);
+                        model.addAttribute("subtask", subTask);
+                        model.addAttribute("error", e.getMessage());
+                        return "pages/Subtask/Edit";
+                }
+
+                redirectAttributes.addFlashAttribute("success", "Sub tugas berhasil diedit");
+                return "redirect:/class/" + classCode + "/tasks/" + taskId + "#subtasks";
+        }
+
 }
